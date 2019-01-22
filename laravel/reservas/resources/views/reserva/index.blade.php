7f27710b75ba2b03876a2b1c67865a937adca487
leyeguemen <46895857+leyeguemen@users.noreply.github.com>
2019-01-22 18:04:11 -0500

    Se le da estilo a la tabla de reservas
    Se aplica bootstrap, a la tabla donde aparece el listado de reservas

--- laravel/reservas/resources/views/reserva/index.blade.php
+++ laravel/reservas/resources/views/reserva/index.blade.php
@@ -2,9 +2,12 @@
 @section('content')
 
 <div class="container-fluid">
-  <h1 class="text-center">Reservas</h1>
-  <a class="btn btn-info" href="{{route('reserva.create')}}">Crear Reserva</a>
-  
+
+    <div class="col-sm-12" >
+      <h1 class="text-center">Reservas</h1>
+      <a class="btn btn-info" href="{{route('reserva.create')}}">Crear Reserva</a>
+    </div>
+
   @if (Session::has('message'))
     <div class="clear-fix" ></div>
     <br/>
@@ -14,46 +17,57 @@
       <strong>Success!</strong> {{ Session::get('message') }} </div>
   @endif
 
-  <div class="clear-fix" ></div>
-  <br/>
-    <table class="table table-condensed">
-    <thead>
-      <tr>
-        <th>Nombre Usuario</th>
-        <th>Apellido Usuario</th>
-        <th>Fecha Reserva</th>
-        <th>Fila</th>
-        <th>Columna</th>
-        <th>Opciones</th>
-      </tr>
-    </thead>
-    <tbody>
+  <div class="col-sm-12" >
+    <div class="clear-fix" ></div>
+    <br/>
+  </div>
 
-        @foreach($reservas as $reserva)
-      <tr>
-        <td>{{$reserva->nombre_usuario}}</td>
-        <td>{{$reserva->apellido_usuario}}</td>
-        <td>{{$reserva->fecha_reserva}}</td>
-        <td>{{$reserva->fila_butaca}}</td>
-        <td>{{$reserva->columna_butaca}}</td>
-        <td>
-          <div class="col-sm-6">
-            <a class="btn btn-success" href="{{route('reserva.edit', $reserva->id)}}">Editar</a>
-          </div>
-          <div class="col-sm-6">
-            <form action="{{ route('reserva.destroy', $reserva->id) }}" method="POST" >
-              @csrf
-              @method('DELETE')
-              <button type="submit" class="btn btn-danger">Eliminar</button>
-            </form>
-          </div>
+  <div class="col-sm-12" >
+  <div class="panel panel-info">
+      <div class="panel-heading">LISTADO DE RESERVAS</div>
+      <div class="panel-body"><table class="table table-bordered table-sm">
+          <thead class="black white-text">
+            <tr>
+              <th class="col-sm-3 text-center">Nombre Usuario</th>
+              <th class="col-sm-3 text-center">Apellido Usuario</th>
+              <th class="col-sm-2 text-center">Fecha Reserva</th>
+              <th class="col-sm-1 text-center">Fila</th>
+              <th class="col-sm-1 text-center">Columna</th>
+              <th class="col-sm-2 text-center">Opciones</th>
+            </tr>
+          </thead>
+          <tbody>
+              @foreach($reservas as $reserva)
+            <tr>
+              <td>{{$reserva->nombre_usuario}}</td>
+              <td>{{$reserva->apellido_usuario}}</td>
+              <td class="text-center">{{$reserva->fecha_reserva}}</td>
+              <td>{{$reserva->fila_butaca}}</td>
+              <td>{{$reserva->columna_butaca}}</td>
+              <td>
+                <div class="col-sm-6">
+                  <a class="btn btn-success" href="{{route('reserva.edit', $reserva->id)}}">Editar</a>
+                </div>
+                <div class="col-sm-6">
+                  <form action="{{ route('reserva.destroy', $reserva->id) }}" method="POST" >
+                    @csrf
+                    @method('DELETE')
+                    <button type="submit" class="btn btn-danger">Eliminar</button>
+                  </form>
+                </div>
+      
+              </td>
+            </tr>
+            @endForeach
+          </tbody>
+        </table>
+        {{$reservas->links()}}
+        </div>
+    </div>
 
-        </td>
-      </tr>
-      @endForeach
-    </tbody>
-  </table>
-  {{$reservas->links()}}
+    
+    
+</div>
 
 </div>
 
